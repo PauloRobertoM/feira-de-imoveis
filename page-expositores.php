c389f6dff7850fa4460a10e8b4bcffa147bf32a1
Paulo Alves <front@agencianovi.com.br>
2019-06-21 14:47:50 -0300

    Expositores finalizada

--- page-expositores.php
+++ page-expositores.php
@@ -2,16 +2,72 @@
 
     <?php include 'components/topo-interna.php'; ?>
 
-    <section class="breads">
+    <section class="menu-geral breads-interna">
         <div class="container">
             <ul>
                 <li><a href="<?php echo site_url('/'); ?>">HOME</a></li>
                 <li><a href="<?php echo site_url('/'); ?>">EXPOSITORES</a></li>
-                <li><a href="<?php echo site_url('/'); ?>">NOTÍCIAS</a></li>
-                <li><a href="<?php echo site_url('/'); ?>">LOCALIZAÇÃO</a></li>
-                <li><a href="<?php echo site_url('/'); ?>">CONTATO</a></li>
             </ul><!-- .nav -->
         </div><!-- container -->
-    </section><!-- breads -->
+    </section><!-- menu-geral -->
+
+    <section class="interna expositores">
+        <div class="container">
+            <h4 class="text-center">15 A 19 de Agosto • Centro de Eventos FIEP</h4>
+            <h1 class="text-center">Os melhores expositores do Brasil estão aqui</h1>
+            <p class="text-center">Na edição 2018, a Feira de Imóveis do Paraná vai contar com diversos expositores, entre construtoras, imobiliárias, incorporadoras e empresas relacionadas à construção civil.</p>
+            <div class="btns">
+                <a href="">mapa da feira</a>
+                <a href="">patrocine a feira</a>
+            </div><!-- btns -->
+            <div class="row">
+                <div class="col-md-3 col-sm-3">
+                    <div class="item">
+                        <a href="">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noticia.jpg" alt="">
+                            <h4>Ademilar</h4>
+                        </a>
+                        <a href="" class="btn-mais" target="_blank">ACESSAR SITE</a>
+                    </div><!-- item -->
+                </div><!-- md-3 -->
+                <div class="col-md-3 col-sm-3">
+                    <div class="item">
+                        <a href="">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noticia.jpg" alt="">
+                            <h4>Ademilar</h4>
+                        </a>
+                        <a href="" class="btn-mais" target="_blank">ACESSAR SITE</a>
+                    </div><!-- item -->
+                </div><!-- md-3 -->
+                <div class="col-md-3 col-sm-3">
+                    <div class="item">
+                        <a href="">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noticia.jpg" alt="">
+                            <h4>Ademilar</h4>
+                        </a>
+                        <a href="" class="btn-mais" target="_blank">ACESSAR SITE</a>
+                    </div><!-- item -->
+                </div><!-- md-3 -->
+                <div class="col-md-3 col-sm-3">
+                    <div class="item">
+                        <a href="">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noticia.jpg" alt="">
+                            <h4>Ademilar</h4>
+                        </a>
+                        <a href="" class="btn-mais" target="_blank">ACESSAR SITE</a>
+                    </div><!-- item -->
+                </div><!-- md-3 -->
+                <div class="col-md-3 col-sm-3">
+                    <div class="item">
+                        <a href="">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noticia.jpg" alt="">
+                            <h4>Ademilar</h4>
+                        </a>
+                        <a href="" class="btn-mais" target="_blank">ACESSAR SITE</a>
+                    </div><!-- item -->
+                </div><!-- md-3 -->
+            </div><!-- row -->
+        </div><!-- container -->
+    </section><!-- interna -->
 
 <?php get_footer(); ?>
