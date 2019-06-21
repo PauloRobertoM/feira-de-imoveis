eb24d6392866f9617569edb217c7a19ef2a955f4
Paulo Alves <front@agencianovi.com.br>
2019-06-21 20:42:50 -0300

    Programação Finalizada

--- page-programacao.php
+++ page-programacao.php
@@ -6,49 +6,57 @@
         <div class="container">
             <ul>
                 <li><a href="<?php echo site_url('/'); ?>">HOME</a></li>
-                <li><a href="<?php echo site_url('/'); ?>">EXPOSITORES</a></li>
+                <li><a href="<?php echo site_url('/'); ?>">PROGRAMAÇÃO</a></li>
             </ul><!-- .nav -->
         </div><!-- container -->
     </section><!-- menu-geral -->
 
-    <section class="interna a-feira">
+    <section class="interna programacao">
         <div class="container">
             <h4 class="text-center">15 A 19 de Agosto • Centro de Eventos FIEP</h4>
-            <h1 class="text-center">Há 27 anos a maior Feira de Imóveis do Paraná</h1>
-            <p class="text-center">Está chegando e você não pode perder, são diversos expositores entre as empresas mais renomadas do país em um só lugar, trazendo para você os melhores imóveis com as condições mais improváveis que você pode encontrar hoje no mercado. Traga sua família e aproveite toda a estrutura que montamos para o seu dia ser confortável com nossa área de recreação e Food Trucks para todas as idades e gostos. Não perca, a entrada e o estacionamento são gratuitos.</p>
+            <h1 class="text-center">Palestras, recreação e muito mais na programação da Feira de Imóveis 2018</h1>
+            <p class="text-center">A 27ª Feira de Imóveis do Paraná 2018, que acontece de 15 a 19 de agosto, no Centro de Eventos da Fiep, na Av. das Torres, em Curitiba, terá PALESTRAS E SHOWS GRATUITOS. Programe-se e participe!</p>
             <div class="row">
-                <div class="col-md-6 col-sm-6">
+                <div class="col-md-4 col-sm-4">
                     <div class="item">
+                        <h3>15.09 • Sexta-Feira</h3>
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noticia.jpg" alt="">
-                        <div class="conteudo">
-                            <h5>ENCONTRE NA FEIRA O IMÓVEL DOS SEUS SONHOS</h5>
-                        </div><!-- conteudo -->
+                        <h2>Marcos Kahtalian</h2>
+                        <p>O novo consumidor de imóveis: as tendências e mudanças no cenário residencial brasileiro.</p>
                     </div><!-- item -->
-                </div><!-- md-6 -->
-                <div class="col-md-6 col-sm-6">
+                </div><!-- md-4 -->
+                <div class="col-md-4 col-sm-4">
                     <div class="item">
+                        <h3>15.09 • Sexta-Feira</h3>
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noticia.jpg" alt="">
-                        <div class="conteudo">
-                            <h5>ENCONTRE NA FEIRA O IMÓVEL DOS SEUS SONHOS</h5>
-                        </div><!-- conteudo -->
+                        <h2>Marcos Kahtalian</h2>
+                        <p>O novo consumidor de imóveis: as tendências e mudanças no cenário residencial brasileiro.</p>
                     </div><!-- item -->
-                </div><!-- md-6 -->
-                <div class="col-md-6 col-sm-6">
+                </div><!-- md-4 -->
+                <div class="col-md-4 col-sm-4">
                     <div class="item">
+                        <h3>15.09 • Sexta-Feira</h3>
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noticia.jpg" alt="">
-                        <div class="conteudo">
-                            <h5>ENCONTRE NA FEIRA O IMÓVEL DOS SEUS SONHOS</h5>
-                        </div><!-- conteudo -->
+                        <h2>Marcos Kahtalian</h2>
+                        <p>O novo consumidor de imóveis: as tendências e mudanças no cenário residencial brasileiro.</p>
                     </div><!-- item -->
-                </div><!-- md-6 -->
-                <div class="col-md-6 col-sm-6">
+                </div><!-- md-4 -->
+                <div class="col-md-4 col-sm-4">
                     <div class="item">
+                        <h3>15.09 • Sexta-Feira</h3>
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noticia.jpg" alt="">
-                        <div class="conteudo">
-                            <h5>ENCONTRE NA FEIRA O IMÓVEL DOS SEUS SONHOS</h5>
-                        </div><!-- conteudo -->
+                        <h2>Marcos Kahtalian</h2>
+                        <p>O novo consumidor de imóveis: as tendências e mudanças no cenário residencial brasileiro.</p>
                     </div><!-- item -->
-                </div><!-- md-6 -->
+                </div><!-- md-4 -->
+                <div class="col-md-4 col-sm-4">
+                    <div class="item">
+                        <h3>15.09 • Sexta-Feira</h3>
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noticia.jpg" alt="">
+                        <h2>Marcos Kahtalian</h2>
+                        <p>O novo consumidor de imóveis: as tendências e mudanças no cenário residencial brasileiro.</p>
+                    </div><!-- item -->
+                </div><!-- md-4 -->
             </div><!-- row -->
         </div><!-- container -->
     </section><!-- interna -->
