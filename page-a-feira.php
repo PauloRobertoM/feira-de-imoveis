019db79582d0a4be57cc764db119dd64dfe83b7c
Paulo Alves <front@agencianovi.com.br>
2019-06-21 20:23:50 -0300

    A Feira Finalizada

---
+++ page-a-feira.php
@@ -0,0 +1,56 @@
+<?php get_header(); ?>
+
+    <?php include 'components/topo-interna.php'; ?>
+
+    <section class="menu-geral breads-interna">
+        <div class="container">
+            <ul>
+                <li><a href="<?php echo site_url('/'); ?>">HOME</a></li>
+                <li><a href="<?php echo site_url('/'); ?>">EXPOSITORES</a></li>
+            </ul><!-- .nav -->
+        </div><!-- container -->
+    </section><!-- menu-geral -->
+
+    <section class="interna a-feira">
+        <div class="container">
+            <h4 class="text-center">15 A 19 de Agosto • Centro de Eventos FIEP</h4>
+            <h1 class="text-center">Há 27 anos a maior Feira de Imóveis do Paraná</h1>
+            <p class="text-center">Está chegando e você não pode perder, são diversos expositores entre as empresas mais renomadas do país em um só lugar, trazendo para você os melhores imóveis com as condições mais improváveis que você pode encontrar hoje no mercado. Traga sua família e aproveite toda a estrutura que montamos para o seu dia ser confortável com nossa área de recreação e Food Trucks para todas as idades e gostos. Não perca, a entrada e o estacionamento são gratuitos.</p>
+            <div class="row">
+                <div class="col-md-6 col-sm-6">
+                    <div class="item">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noticia.jpg" alt="">
+                        <div class="conteudo">
+                            <h5>ENCONTRE NA FEIRA O IMÓVEL DOS SEUS SONHOS</h5>
+                        </div><!-- conteudo -->
+                    </div><!-- item -->
+                </div><!-- md-6 -->
+                <div class="col-md-6 col-sm-6">
+                    <div class="item">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noticia.jpg" alt="">
+                        <div class="conteudo">
+                            <h5>ENCONTRE NA FEIRA O IMÓVEL DOS SEUS SONHOS</h5>
+                        </div><!-- conteudo -->
+                    </div><!-- item -->
+                </div><!-- md-6 -->
+                <div class="col-md-6 col-sm-6">
+                    <div class="item">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noticia.jpg" alt="">
+                        <div class="conteudo">
+                            <h5>ENCONTRE NA FEIRA O IMÓVEL DOS SEUS SONHOS</h5>
+                        </div><!-- conteudo -->
+                    </div><!-- item -->
+                </div><!-- md-6 -->
+                <div class="col-md-6 col-sm-6">
+                    <div class="item">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/noticia.jpg" alt="">
+                        <div class="conteudo">
+                            <h5>ENCONTRE NA FEIRA O IMÓVEL DOS SEUS SONHOS</h5>
+                        </div><!-- conteudo -->
+                    </div><!-- item -->
+                </div><!-- md-6 -->
+            </div><!-- row -->
+        </div><!-- container -->
+    </section><!-- interna -->
+
+<?php get_footer(); ?>
